bac99e6a23acf395e092722be343f40c8e6cce37
Rayssa Ellen <65548341+ray396@users.noreply.github.com>
2023-05-02 16:57:14 -0300

    novo projeto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { HeaderContainer } from "./styles";
 import logoIgnite from "../../assets/logo-ignite.svg";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
     return (
         <HeaderContainer>
             <img src={logoIgnite} alt=""/>
             <nav>
-                <a href="">timer</a>
-                <a href="">history</a>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/history" title="historico">
+                    <Scroll size={24}/>
+                </NavLink>
             </nav>
         </HeaderContainer>
     )
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export function Home() {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  )
-}
